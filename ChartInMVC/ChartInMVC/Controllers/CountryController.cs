182b6ec695e7ce18770607eb1f5ea10ef25fbe33
Touhidul Fahim <muhdtohidul@gmail.com>
2018-08-10 03:26:55 +0600

    Add Country

---
+++ ChartInMVC/ChartInMVC/Controllers/CountryController.cs
@@ -0,0 +1,39 @@
+﻿using ChartInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartInMVC.Controllers
+{
+    public class CountryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        
+        public ActionResult ViewCountry()
+        {
+            return View(db.Country.OrderBy(d => d.CountryName).ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddCountry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddCountry(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Country.Add(country);
+                db.SaveChanges();
+                return RedirectToAction("ViewCountry");
+            }
+
+            return View(country);
+        }
+    }
+}
