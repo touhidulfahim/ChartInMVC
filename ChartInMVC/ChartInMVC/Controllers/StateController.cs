f459b5ba845c4cc79b56aec9d5e3156922fb7fab
Touhidul Fahim <muhdtohidul@gmail.com>
2018-08-10 04:19:34 +0600

    Add State

---
+++ ChartInMVC/ChartInMVC/Controllers/StateController.cs
@@ -0,0 +1,38 @@
+﻿using ChartInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartInMVC.Controllers
+{
+    public class StateController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult ViewState()
+        {
+            return View(db.State.OrderBy(a=>a.StateName).ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddState()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddState(State state)
+        {
+            if (ModelState.IsValid)
+            {
+                db.State.Add(state);
+                db.SaveChanges();
+                return RedirectToAction("ViewState");
+            }
+            return View(state);
+        }
+    }
+}
