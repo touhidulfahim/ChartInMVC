83fbd59154d8da2c29302b2268530db04d967d33
Touhidul Fahim <muhdtohidul@gmail.com>
2018-08-10 16:16:32 +0600

    Add Product

---
+++ ChartInMVC/ChartInMVC/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ChartInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartInMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult ViewProduct()
+        {
+            return View(db.Product.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("ViewProduct");
+            }
+            return View();
+        }
+    }
+}
